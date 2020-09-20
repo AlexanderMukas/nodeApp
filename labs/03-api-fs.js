625ce2605d425b82aa2266074099edd0a49e3faa
AlexanderMukas <alexandermukas@yahoo.com>
2020-09-20 17:02:54 +0300

    review fs node commit

--- labs/03-api-fs.js
+++ labs/03-api-fs.js
@@ -1,5 +1,6 @@
 // const { stat, writeFile} = require('fs');
-const testDir = '/home/mukas/coding/sql'
+const testDir = '/home/mukas/coding/sql';
+const filePath = '/home/mukas/coding/sql/auth_book_insert.txt';
 const fs = require('fs');
 
 // console.log(fs);
@@ -11,14 +12,24 @@ const fs = require('fs');
 //     console.log(stats.blocks);
 // });
 
+// содержимое директории
 fs.readdir(testDir, (err, files) => {
     if(err){
         console.error(err);
-        return
+        return;
     };
     console.log(`${testDir}:`);
     files.forEach( file => {
         console.log(`\t\t\t ${file}`);
-    })
+    });
+});
 
-})
+// статус файла
+fs.stat(filePath, (err, stats) => {
+    if(err){
+        console.error(err);
+        return;
+    };
+    console.log(`dir: ${filePath} \nfile: ${stats.isFile()}\n${stats.size} bytes`);
+});
+// check pls - habr.com/ru/company/ruvds/blog/424969
